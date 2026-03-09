d2af53a56ba74899af9ca138aab76d03c7929aa7
MLUTHF11 <mluthfikuu@gmail.com>
2026-03-09 14:33:34 +0700

    4 pilar OOP menggunakan java

--- ti_desain_pattern/Module_3/Bagian_6/Pesanan.java
+++ ti_desain_pattern/Module_3/Bagian_6/Pesanan.java
@@ -3,4 +3,35 @@ package Bagian_6;
 public class Pesanan {
     private final String namaPemesan;
     private final Tiket tiket;
+    private final int jumlah;
+
+    public Pesanan(String namaPemesan, Tiket tiket, int jumlah){
+        this.namaPemesan = namaPemesan;
+        this.tiket = tiket;
+        this.jumlah = jumlah;
+    }
+
+    public String getNamaPemesan() {
+        return namaPemesan;
+    }
+
+    public Tiket getTiket() {
+        return tiket;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+    public double hitungTotal(){
+        double total = tiket.getHarga() * jumlah;
+        double diskon = tiket.hitungDiskon() * jumlah;
+        return total - diskon;
+    }
+    public void displayDetail(){
+        System.out.println("\nDetail Pesanan");
+        System.out.println("Nama Pemesan: "+ namaPemesan);
+        System.out.println("Jenis tiket: "+ tiket.getJenis());
+        System.out.println("Jumlah: "+ jumlah);
+        System.out.println("Total Harga: RP"+ hitungTotal());
+    }
 }
